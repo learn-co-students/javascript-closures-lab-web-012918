48a33594d970ea3f4c5928ec1f940727fe12c22b
harenarium <22226614+harenarium@users.noreply.github.com>
2018-03-13 08:47:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  let counter = 0
+  function addBump(){
+    return ++counter
+  }
+  function getBumps(){
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType){
+  return function completeAnimal(deadlyDevice){
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  }
+}
+
+const sharkCreator = createAnimal("Shark")
+const sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+const sharkWithFrickinCannon = sharkCreator("Cannon")
