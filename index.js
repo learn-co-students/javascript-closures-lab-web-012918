7f52bc492a49ece33f5ffcf1dff5347043b68dac
alin684 <al684@cornell.edu>
2018-03-16 16:10:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  let counter = 0
+  function addBump(){
+    counter += 1
+  }
+  function getBumps(){
+    return counter
+  }
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType){
+  function innerFunc(deadlyDevice) {
+    return {
+      animalType: `${animalType}`,
+      deadlyDevice: `${deadlyDevice}`
+    }
+  }
+  return innerFunc
+}
+let sharkCreator = createAnimal("Shark")
+let sharkWithFrickinLaserbeam = createAnimal("Shark")("Laserbeam")
+let sharkWithFrickinCannon = createAnimal("Shark")("Cannon")
